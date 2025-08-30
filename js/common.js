895737af35a63cb1f6f1becb9ba5127949f5fa9a
noxxasoloera <bankzonk02@gmail.com>
2025-08-31 05:59:22 +0800

    Delete js directory

--- js/common.js
+++
@@ -1,9 +0,0 @@
-let current = JSON.parse(localStorage.getItem("currentUser") || "null");
-
-function canCreateRole(role,target){
-  if(role==='owner') return true;
-  if(role==='CEO') return target!=='owner';
-  if(role==='mods') return ['partner','reseller'].includes(target);
-  if(role==='partner') return target==='reseller';
-  return false;
-}
